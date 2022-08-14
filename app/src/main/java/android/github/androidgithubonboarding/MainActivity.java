1976c99d4623cde177fb28ed64f30bb206e1f397
sidnaga <sidnaga@umich.edu>
2022-08-14 14:13:08 -0400

    initial setup

--- app/src/main/java/android/github/androidgithubonboarding/MainActivity.java
+++ app/src/main/java/android/github/androidgithubonboarding/MainActivity.java
@@ -3,6 +3,12 @@ package android.github.androidgithubonboarding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mainButton = findViewById(R.id.generate);
+        ArrayList names = new ArrayList<String>();
+        //Todo add name to names arraylist like in line 22
+        names.add("Siddharth");
+        Random rand = new Random();
+        int random_number = rand.nextInt(names.size());
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = (String) names.get(random_number);
+                Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
